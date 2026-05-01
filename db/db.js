fd9e2a5df94ffa5195e88a87291f4c8decfec39b
ravi <ravish1086@gmail.com>
2026-05-01 13:33:30 +0530

    refactor: update MongoDB connection options for improved configuration

--- db/db.js
+++ db/db.js
@@ -2,13 +2,10 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
 const connectDB = async () => {
     try {
-      await mongoose.connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      });
+      await mongoose.connect(process.env.MONGO_URI, clientOptions);
       console.log('MongoDB connected');
     } catch (error) {
       console.error(`Error: ${error.message}`);
